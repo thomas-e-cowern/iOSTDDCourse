e4fe2efe59caa2c098cacbe52f96cbca8a2a071a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-27 15:01:38 -0400

    created film fest project

---
+++ FilmFest/FilmFest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmFest
+//
+//  Created by Thomas Cowern New on 7/27/20.
+//  Copyright © 2020 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
