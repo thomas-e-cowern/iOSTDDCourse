6ea8e1bf58b2ba898ba0a2866757fb074a3c829e
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-07-30 16:17:46 -0400

    Added section 0 test

--- FilmFest/FilmFestTests/Test Data Service/MovieLibraryDataServiceTests.swift
+++ FilmFest/FilmFestTests/Test Data Service/MovieLibraryDataServiceTests.swift
@@ -8,26 +8,46 @@
 
 import XCTest
 
+@testable import FilmFest
 class MovieLibraryDataServiceTests: XCTestCase {
 
+    var sut: MovieLibraryDataService!
+    var libraryTableView: UITableView!
+    
+    let fairytale = Movie(title: "Fairy Tale")
+    let thriller = Movie(title: "Thriller")
+    let darkComedy = Movie(title: "Dark Comdedy")
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = MovieLibraryDataService()
+        sut.movieManager = MovieManager()
+        libraryTableView = UITableView()
+        libraryTableView.dataSource = sut
+        libraryTableView.delegate = sut
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // MARK: Sections
+    func testTableViewSections_Count_ReturnsTwo () {
+        let sections = libraryTableView.numberOfSections
+        XCTAssertEqual(sections, 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testTableViewSections_SectionOne_ReturnsMoviesToSeeCount () {
+        sut.movieManager?.addMovie(movie: fairytale)
+        sut.movieManager?.addMovie(movie: darkComedy)
+        
+        XCTAssertEqual(libraryTableView.numberOfRows(inSection: 0), 2)
+        
+        sut.movieManager?.addMovie(movie: thriller)
+        libraryTableView.reloadData()
+        
+        XCTAssertEqual(libraryTableView.numberOfRows(inSection: 0), 3)
     }
-
 }
+
